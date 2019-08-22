7b800f1ab07beeb0c2169f174050c2a78386870c
Gorbunova Kristina <gorbunova_kris4@mail.ru>
2019-08-22 20:56:22 +0300

    added hash]

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [
@@ -18,13 +19,14 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'index.css'
+            filename: 'style.[contenthash].css'
         }),
         new HtmlWebpackPlugin({
             inject: false,
             hash: true,
             template: './src/index.html',
             filename: 'index.html'
-        })
+        }),
+        new WebpackMd5Hash()
     ]
 };
