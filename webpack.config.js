d9cf32f768ff773fe96ca9c0c910ac8f66e1199e
Gorbunova Kristina <gorbunova_kris4@mail.ru>
2019-08-22 20:50:43 +0300

    configured html and css in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
-}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'index.css'
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
